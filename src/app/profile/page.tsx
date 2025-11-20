117043a822d4c7e7acbde520a87d5cda31405971
dliubich <detmir-group\DLiubich>
2025-11-20 22:19:33 +0300

    authorization: authConfig was separated from route.ts

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/features/api/auth/authConfig';
 import { redirect } from 'next/navigation';
 
 const Profile = async () => {
